78c071024a03a77231ff6167ac5f87d5d4ad5c91
yurii93 <yura.vashchenko@gmail.com>
2021-07-27 19:25:09 +0300

    Initial simple web app commit

---
+++ src/main/java/com/yuravashchenko/spring/mvc_hibernate_aop/controller/MyController.java
@@ -0,0 +1,55 @@
+package com.yuravashchenko.spring.mvc_hibernate_aop.controller;
+
+import com.yuravashchenko.spring.mvc_hibernate_aop.entity.Employee;
+import com.yuravashchenko.spring.mvc_hibernate_aop.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    private final EmployeeService employeeService;
+
+    public MyController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @RequestMapping("/")
+    public String showAllEmployees(Model model) {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+        return "all-employees";
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+        employeeService.deleteEmployee(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/yuravashchenko/spring/mvc_hibernate_aop/dao/EmployeeDaoImpl.java
@@ -0,0 +1,46 @@
+package com.yuravashchenko.spring.mvc_hibernate_aop.dao;
+
+import com.yuravashchenko.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDaoImpl implements EmployeeDao {
+
+    private final SessionFactory sessionFactory;
+
+    public EmployeeDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Employee", Employee.class).getResultList();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee where id = :empId");
+        query.setParameter("empId", id);
+        query.executeUpdate();
+    }
+}
